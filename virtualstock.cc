0491e7bac89b201128085860cd24a873c845c47a
Mohammed Chaib <mohammedchaib2008@outlook.com>
2026-08-13 14:53:23 +0200

    added more companies

--- virtualstock.cc
+++ virtualstock.cc
@@ -3,17 +3,19 @@
 #include <string_view>
 #include <time.h>
 #include <unistd.h>
+#include <iomanip>
 using namespace std;
 
 
 int main() {
 string change_type;
-string company_list[4]{"OMNI","Z","HYDR","CHIP"};
-float prices[4]{100,100,100,100};
+string company_list[20]{"OMNI","Z","HYDR","CHIP","NOVA","VOLT","AERO","CYBR","LUMN","VRTX","NEXA","ORBT","QUAD","SYNE","PRMX","EVRA","AXIS","KORP","FLUX","TRNX"};
+float prices[20]{100,75,250,120,180,90,340,65,210,150,45,300,110,80,175,60,225,130,95,270};
+srand(time(NULL));
 while (1==1)
 {
 float random_change = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) *10;
-int random_company = rand() % 4;
+int random_company = rand() % 20;
 int decline_increase = rand() % 2;
 
 
@@ -28,18 +30,20 @@ if (decline_increase == 1)
     prices[random_company] *= (1 - random_change/100);
 }
 
-cout << "\033[2J\033[H";
-    cout << "Market\n\n";
-    cout << "OMNI: " << prices[0] << "\n";
-    cout << "Z:    " << prices[1] << "\n";
-    cout << "HYDR: " << prices[2] << "\n";
-    cout << "CHIP: " << prices[3] << "\n";
+cout << "\033[H"; 
 
-    cout << "last change: ";
-    cout << company_list[random_company] <<": "<< change_type << random_change << "% "<< "\n";
-    
-    cout << flush;
-    sleep(1);
+
+for (size_t i = 0; i < 20; i++)
+{
+    cout << "Market\n\n";
+    cout << company_list[i] << ": " << prices[i] << "\n";
+}
+cout << "\nlast change: ";
+cout << fixed << showpoint;
+cout << setprecision(2);
+cout << company_list[random_company] << ": " << change_type << random_change;
+
+sleep(1);
 }
 
 
